8042c71d2ac8af13231db17d1d95c18853844144
sky59 <sky59@users.noreply.github.com>
2021-10-21 08:09:56 +0200

    Add files via upload

---
+++ HashTable.h
@@ -0,0 +1,106 @@
+/*
+ * This file is part of the BSGS distribution (https://github.com/JeanLucPons/Kangaroo).
+ * Copyright (c) 2020 Jean Luc PONS.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef HASHTABLEH
+#define HASHTABLEH
+
+#include <string>
+#include <vector>
+#include "SECPK1/Point.h"
+#include "Constants.h"
+#ifdef WIN64
+#include <Windows.h>
+#endif
+
+#define HASH_SIZE_BIT 18
+#define HASH_SIZE (1<<HASH_SIZE_BIT)
+#define HASH_MASK (HASH_SIZE-1)
+
+#define ADD_OK        0
+#define ADD_DUPLICATE 1
+#define ADD_COLLISION 2
+
+union int256_s {
+        uint8_t  i8[32];
+        uint16_t i16[16];
+        uint32_t i32[8];
+        uint64_t i64[4];
+};
+
+
+typedef union int256_s int256_t;
+
+#define safe_free(x) if(x) {free(x);x=NULL;}
+
+// We store only 128 (+18) bit a the x value which give a probabilty a wrong collision after 2^73 entries
+
+typedef struct {
+
+  int256_t  x;    // Position of kangaroo (256bit LSB)
+  int256_t  d;    // Travelled distance (b255=sign b254=kangaroo type, b253..b0 distance
+
+} ENTRY;
+
+typedef struct {
+
+  uint32_t   nbItem;
+  uint32_t   maxItem;
+  ENTRY    **items;
+
+} HASH_ENTRY;
+
+class HashTable {
+
+public:
+
+  HashTable();
+  int Add(Int *x,Int *d,uint32_t type);
+  int Add(uint64_t h,int256_t *x,int256_t *d);
+  int Add(uint64_t h,ENTRY *e);
+  uint64_t GetNbItem();
+  void Reset();
+  std::string GetSizeInfo();
+  void PrintInfo();
+  void SaveTable(FILE *f);
+  void SaveTable(FILE* f,uint32_t from,uint32_t to,bool printPoint=true);
+  void LoadTable(FILE *f);
+  void LoadTable(FILE* f,uint32_t from,uint32_t to);
+  void ReAllocate(uint64_t h,uint32_t add);
+  void SeekNbItem(FILE* f,bool restorePos = false);
+  void SeekNbItem(FILE* f,uint32_t from,uint32_t to);
+
+  HASH_ENTRY    E[HASH_SIZE];
+  // Collision info
+  Int      kDist;
+  uint32_t kType;
+
+  static void Convert(Int *x,Int *d,uint32_t type,uint64_t *h,int256_t *X,int256_t *D);
+  static int MergeH(uint32_t h,FILE* f1,FILE* f2,FILE* fd,uint32_t *nbDP,uint32_t* duplicate,
+                    Int* d1,uint32_t* k1,Int* d2,uint32_t* k2);
+  static void CalcDistAndType(int256_t d,Int* kDist,uint32_t* kType);
+  //static void toint256t(Int *a, int256_t *b);                           // sky59
+  //static void toInt(int256_t *a, Int *b);                               // mozno to bude treba
+
+private:
+
+  ENTRY *CreateEntry(int256_t *x,int256_t *d);
+  static int compare(int256_t *i1,int256_t *i2);
+  std::string GetStr(int256_t *i);
+
+};
+
+#endif // HASHTABLEH
---
+++ readme.txt
@@ -0,0 +1,27 @@
+This 254bit version is based on:
+
+https://github.com/JeanLucPons/Kangaroo        125bit version
+
+with ideas from:
+
+https://github.com/ZenulAbidin/Kangaroo-256    (said to be buggy and not finished)
+
+
+Compilation:
+
+Download 125b version from JLP
+replace files from here and compile
+
+
+Notes:
+
+Kangaroo254.exe is compiled only for CPU  (no GPU support), try -check switch
+
+DP size is left untouched, 64bits contrary to 256 version with 256bit DP mask
+hopefully it is long enough
+
+Search interval is 254bits
+
+In files here you can search for keyword "sky59", I commented almost everything
+what has been changed  (no minor and clear changes)
+Can even compare original files with these new files, tripple check is always welcome!
